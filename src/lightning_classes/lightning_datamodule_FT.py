8455294bbcec762fdcb0c377c3dc806219476cea
yizhilll <yizhi.li@hotmail.com>
2024-02-01 04:10:05 +0000

    codes

--- src/lightning_classes/lightning_datamodule_FT.py
+++ src/lightning_classes/lightning_datamodule_FT.py
@@ -1,92 +1,132 @@
-#!/usr/bin/env python  
-# -*- coding:utf-8 -*-  
-""" 
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+"""
 @author: Siwei Wu
-@file: lightning_datamodule_HRQuery.py 
+@file: data_load_for_HRQuery.py
 @time: 2022/08/10
 @contact: wusiwei@njust.edu.cn
 """
-import logging
+import json
+
 import pytorch_lightning as pl
+import random
+import csv
+from torch.utils.data.dataset import Dataset
+from dataclasses import dataclass
+# from transformers import BertTokenizer , BertModel
 import torch
-from src.data_load.data_load_for_FT import SciMMIR_FT_Dataset
-from torch.utils.data import DataLoader
+import logging
+import copy
+from PIL import Image
+import datasets
+from tqdm import tqdm
 
-class SciMMIR_FT_DataMoudle(pl.LightningDataModule):
-    def __init__(self, config , tokenizer, preprocess):
-        super(SciMMIR_FT_DataMoudle, self).__init__()
-        self.config = config
-        self.tokenizer = tokenizer
+@dataclass(frozen = True)
+class InputExample:
+    Query: str
+    Fig: str
+    Fig_num: int
+    Text_num: int
+    Image_type : str
+
+def load_json(path):
+    f = open(path , 'r')
+    data = json.load(f)
+    f.close()
+
+    return data
+
+class SciMMIR_FT_Dataset(Dataset):
+    def __init__(
+            self,
+            tokenizer,
+            preprocess,
+            mode: str,
+            context_length: int,
+            text_2_image_index: str,
+            text_2_index: str,
+            config,
+    ):
+        super(SciMMIR_FT_Dataset , self).__init__()
+        # process = DataProcess()
         self.preprocess = preprocess
+        self.tokenizer = tokenizer
+        self.mode = mode
+        self.context_length = context_length
+        self.text_2_image_index = load_json(text_2_image_index)
+        self.text_2_index = load_json(text_2_index)
+        self.config = config
+        self.selected_training_samples_index = {}
 
-    def setup(self, stage = None):
-        if stage == 'train' or stage == None:
-            self.train_dataset = SciMMIR_FT_Dataset(
-                tokenizer = self.tokenizer,
-                preprocess = self.preprocess,
-                mode = 'train',
-                context_length = self.config.context_length,
-                text_2_image_index = self.config.datasets_saved_path + self.config.text_2_image_index_valid,
-                text_2_index = self.config.datasets_saved_path + self.config.text_2_index_valid,
-                config = self.config,
-            )
 
-            self.test_dataset = SciMMIR_FT_Dataset(
-                tokenizer = self.tokenizer,
-                preprocess = self.preprocess,
-                mode = 'test',
-                context_length = self.config.context_length,
-                text_2_image_index = self.config.datasets_saved_path + self.config.text_2_image_index_test,
-                text_2_index = self.config.datasets_saved_path + self.config.text_2_index_test,
-                config = self.config,
-            )
+        ds_remote = datasets.load_dataset("m-a-p/SciMMIR" )
+        if mode == 'train':
+            self.examples = ds_remote['train']
+            self.examples = self.examples
 
-            self.val_dataset = SciMMIR_FT_Dataset(
-                tokenizer = self.tokenizer,
-                preprocess = self.preprocess,
-                mode = 'valid',
-                context_length = self.config.context_length,
-                text_2_image_index = self.config.datasets_saved_path + self.config.text_2_image_index_valid,
-                text_2_index = self.config.datasets_saved_path + self.config.text_2_index_valid,
-                config = self.config,
-            )
+            if self.config.image_type != 'overall':
+                self.selected_training_samples_index = load_json('./data/training_samples_image_type.json')
+            logging.info("Current examples: %s" , len(self.examples))
 
-        elif stage == 'test':
-            self.test_dataset = SciMMIR_FT_Dataset(
-                tokenizer = self.tokenizer,
-                preprocess = self.preprocess,
-                mode = 'test',
-                context_length = self.config.context_length,
-                text_2_image_index = self.config.datasets_saved_path + self.config.text_2_image_index_test,
-                text_2_index = self.config.datasets_saved_path + self.config.text_2_index_test,
-                config = self.config,
-            )
-    def prepare_data(self):
-        logging.info(f"nothing")
+        elif mode == 'test':
+            self.examples = ds_remote['test']
+            logging.info("Current examples: %s" , len(self.examples))
+        elif mode == 'valid':
+            self.examples = ds_remote['validation']
 
-    def train_dataloader(self):
-        return DataLoader(
-            self.train_dataset,
-            batch_size= self.config.train_batch_size,
-            num_workers= 32,
-            pin_memory= True,
-            shuffle= True,
-        )
+            logging.info("Current examples: %s" , len(self.examples))
 
-    def test_dataloader(self):
-        return DataLoader(
-            self.test_dataset,
-            batch_size=self.config.test_batch_size,
-            num_workers=16,
-            pin_memory=True,
-            shuffle=False
-        )
+    def __getitem__(self, index):
+        if self.mode == 'train' and self.config.image_type != 'overall':
+            example = self.examples[self.selected_training_samples_index[self.config.image_type][str(index)]]
+        else:
+            example = self.examples[index]
+        Query = example['text']
+        Fig = example['image']
+        if self.mode in ['test', 'valid']:
+            Fig_num = self.text_2_image_index[Query]
+            Text_num = self.text_2_index[Query]
+        else:
+            Fig_num = 0
+            Text_num = 0
+        Image_type = example['class']
 
-    def val_dataloader(self):
-        return DataLoader(
-            self.val_dataset,
-            batch_size=self.config.val_batch_size,
-            num_workers=16,
-            pin_memory=False,
-        )
+        if self.config.Use_BERT == True:
+            Q_token = self.tokenizer(Query, add_special_tokens=True,padding='max_length', truncation=True, max_length = self.context_length, return_tensors = 'pt')
+        else:
+            if self.config.model_name == 'CLIP':
+                Q_token = self.tokenizer(Query, context_length=self.context_length, truncate = True)
+                Q_token = Q_token.squeeze(1)
+            elif self.config.model_name == 'BLIP' or self.config.model_name == 'BLIP-large':
+                Q_token = self.preprocess(text = Query, add_special_tokens=True,padding='max_length', truncation=True, max_length = self.context_length, return_tensors = 'pt')
 
+        if self.config.model_name == 'CLIP':
+            Fig_preprocess = self.preprocess(Fig).unsqueeze(0)
+        elif self.config.model_name == 'BLIP' or self.config.model_name == 'BLIP-large':
+            Fig_preprocess = self.preprocess( images = Fig, return_tensors="pt").pixel_values.squeeze(0)#, dtype=torch.float16
+
+        if self.mode == 'train':
+
+            return {
+                'Q_token': Q_token,
+                'Fig_preprocess': Fig_preprocess,
+                'Fig_num': Fig_num,
+                'Text_num': Text_num,
+                "Image_type": Image_type
+            }
+
+        elif self.mode == 'test' or self.mode == 'valid':
+            return {
+                'Q_token': Q_token,
+                'Fig_preprocess': Fig_preprocess,
+                'Fig_num': Fig_num,
+                'Text_num': Text_num,
+                "Image_type": Image_type
+            }
+
+    def __len__(self):
+        if self.mode == 'train' and self.config.image_type != 'overall':
+            # print('subclass: ', len(self.selected_training_samples_index))
+            return len(self.selected_training_samples_index[self.config.image_type])
+        else:
+            return self.examples.num_rows 
